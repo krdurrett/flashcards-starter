fcc7072ddb74533b8286ea413f1edda3c9c349ae
Kayla Durrett <kayla.durrett@gmail.com>
2021-11-29 20:19:28 -0700

    Pass the property Turn class tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,23 +6,23 @@ const Card = require('../src/Card');
 
 describe('Turn', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
-  it.skip('should be an instance of Turn', function() {
+  it('should be an instance of Turn', function() {
     const turn = new Turn();
     expect(turn).to.be.an.instanceOf(Turn);
   });
 
-  it.skip('should store a users guess', function() {
+  it('should store a users guess', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
     expect(turn.guess).to.equal('object');
   });
 
-  it.skip('should store the current card at play', function() {
+  it('should store the current card at play', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
     expect(turn.card).to.be.a('object');
