aacdf021020a3ed1473967517bd7140a7a36c31b
Kayla Durrett <kayla.durrett@gmail.com>
2021-11-29 19:06:56 -0700

    Add inital tests for property to turn test file

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,8 +1,36 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/Card');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
-  
+
+  it.skip('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceOf(Turn);
+  });
+
+  it.skip('should store a users guess', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.guess).to.equal('object');
+  });
+
+  it.skip('should store the current card at play', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.card).to.be.a('object');
+    expect(turn.card).to.deep.equal({
+      "id": 1,
+      "question": "What allows you to define a set of related information using key-value pairs?",
+      "answers": ["object", "array", "function"],
+      "correctAnswer": "object"
+    })
+  });
 })
