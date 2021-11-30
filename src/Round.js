46bcc148a82b8299c392b4a002840369d4c19dbb
Kayla Durrett <kayla.durrett@gmail.com>
2021-11-30 16:59:27 -0700

    Pass the tests for the takeTurn method

--- src/Round.js
+++ src/Round.js
@@ -3,12 +3,23 @@ const Turn = require('../src/Turn');
 class Round {
   constructor(deck) {
     this.deck = deck;
+    this.turns = 0;
+    this.incorrectGuesses = [];
   }
   returnCurrentCard() {
     return this.deck.cards[0];
   }
   takeTurn(guess) {
-    return new Turn(guess, this.returnCurrentCard());
+    let currentCard = this.returnCurrentCard();
+    let turn = new Turn(guess, currentCard);
+    this.turns += 1;
+    this.deck.cards.shift();
+    if(turn.evaluateGuess()) {
+      return 'correct!';
+    } else {
+      this.incorrectGuesses.push(currentCard.id);
+      return 'incorrect!';
+    }
   }
 }
 
