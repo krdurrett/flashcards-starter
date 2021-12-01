68b2e2f01d005f2b1ed812dfd745b51fae3e72bf
Kayla Durrett <kayla.durrett@gmail.com>
2021-12-01 14:33:05 -0700

    Add console log message

--- src/Round.js
+++ src/Round.js
@@ -27,6 +27,7 @@ class Round {
     return Math.round(percentageCorrect);
   }
   endRound() {
+    console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
     return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`;
   }
 }
